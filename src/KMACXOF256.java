4fae50a8d0d71a7179dc96ee00a464afb3ffb702
Joshua Barbee <joshuaa.bbarbee@gmail.com>
2023-03-28 15:14:53 -0700

    Initial

---
+++ src/KMACXOF256.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+
+public class KMACXOF256 {
+
+//    private static byte enc8(final byte bigEndian) {
+//
+//    }
+//
+//    private static int digitsInBase256(final BigInteger x) {
+//
+//    }
+}
