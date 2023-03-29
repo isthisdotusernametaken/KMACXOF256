ed82d782435e31284e68257fdfd0c46922b6b61b
Joshua Barbee <joshuaa.bbarbee@gmail.com>
2023-03-28 22:53:26 -0700

    Completed enc8 Started right_ and left_encode

--- src/KMACXOF256.java
+++ src/KMACXOF256.java
@@ -2,11 +2,30 @@ import java.math.BigInteger;
 
 public class KMACXOF256 {
 
-//    private static byte enc8(final byte bigEndian) {
-//
-//    }
-//
-//    private static int digitsInBase256(final BigInteger x) {
-//
-//    }
+    static void rightEncode(final BigInteger x) {
+        final byte[] bytes = x.toByteArray();
+        reverseByteEndianness(bytes);
+    }
+
+    private static void reverseByteEndianness(final byte[] bytes) {
+        for (int i = 0; i < bytes.length; i++)
+            bytes[i] = enc8(bytes[i]);
+    }
+
+    private static byte enc8(final byte bigEndian) {
+        // Reverse the order of the bits to convert from big-endian to little-endian
+        return (byte) (
+                ((bigEndian & 0b0000_0001) << 7) |  // Move bit 7 to bit 0
+                ((bigEndian & 0b0000_0010) << 5) |  // Bit 6 to bit 1
+                ((bigEndian & 0b0000_0100) << 3) |  // Bit 5 to bit 2
+                ((bigEndian & 0b0000_1000) << 1) |  // Bit 4 to bit 3
+                ((bigEndian & 0b0001_0000) >>> 1) |  // Bit 3 to bit 4
+                ((bigEndian & 0b0010_0000) >>> 3) |  // Bit 2 to bit 5
+                ((bigEndian & 0b0100_0000) >>> 5) |  // Bit 1 to bit 6
+                ((bigEndian & 0b1000_0000) >>> 7)    // Bit 0 to bit 7 — Because this is the only
+                                                        // case where the sign bit is 1, this is
+                                                        // technically the only case that needs an
+                                                        // unsigned right shift
+        );
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        var n = new BigInteger("12313");
+//        var n = new BigInteger("12313");
+//
+//        var bytes = n.toByteArray();
 
-        var reversed = n.toByteArray();
 
     }
 }
