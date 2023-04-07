38dfe0bac0ab9f48e591909531ff35f8bb882647
Joshua Barbee <joshuaa.bbarbee@gmail.com>
2023-04-07 14:21:28 -0700

    Uncommented the UI call lol

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //UserInterface.menuNav();
-        System.out.println(Util.bytesToByteSpacedHexString(KMACXOF256.encodeString(Util.ASCIIStringToBytes("My Tagged Application"))));
+        UserInterface.menuNav();
+        // System.out.println(Util.bytesToByteSpacedHexString(KMACXOF256.encodeString(Util.ASCIIStringToBytes("My Tagged Application"))));
     }
 }
