f0c5a659700aedc4d9e76123a9d624859fb861e9
Joshua Barbee <joshuaa.bbarbee@gmail.com>
2023-05-08 13:33:38 -0700

    Added r constant

--- src/ModularArithmetic.java
+++ src/ModularArithmetic.java
@@ -2,9 +2,15 @@ import java.math.BigInteger;
 
 public class ModularArithmetic {
 
+    private static final BigInteger r = BigInteger.TWO.pow(446)
+                                        .subtract(new BigInteger(
+                                                "138180668098951153520073867" +
+                                                "4851542688033669247488217860989" +
+                                                "4547503885"
+                                        ));
     private static final BigInteger p = BigInteger.TWO.pow(448)
-            .subtract(BigInteger.TWO.pow(224))
-            .subtract(BigInteger.ONE);
+                                        .subtract(BigInteger.TWO.pow(224))
+                                        .subtract(BigInteger.ONE);
 
     static BigInteger add(final BigInteger augend, final BigInteger addend) {
         return augend.add(addend).mod(p);
@@ -24,6 +30,10 @@ public class ModularArithmetic {
 
     static BigInteger div(final BigInteger dividend, final BigInteger divisor) {
         return dividend.multiply(divisor.modInverse(p)).mod(p);
+    }
+
+    static BigInteger modR(final BigInteger val) {
+        return val.mod(r);
     }
 
     static BigInteger sqrt(final BigInteger radicand, final boolean positive) {
